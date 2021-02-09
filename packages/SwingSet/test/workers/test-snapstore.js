35a0ad8c1590fde9d6f71cea6cb12e8249f9fd57
Dan Connolly <connolly@agoric.com>
2021-02-09 10:10:13 -0600

    refactor(swingset): simplify snapstore API

--- packages/SwingSet/test/workers/test-snapstore.js
+++ packages/SwingSet/test/workers/test-snapstore.js
@@ -2,7 +2,6 @@
 
 import '@agoric/install-ses';
 import { spawn } from 'child_process';
-import { createGzip } from 'zlib';
 import { type as osType } from 'os';
 import fs from 'fs';
 import path from 'path';
@@ -11,11 +10,7 @@ import test from 'ava';
 import tmp from 'tmp';
 import { xsnap } from '@agoric/xsnap';
 import bundleSource from '@agoric/bundle-source';
-import {
-  makeSnapstore,
-  saveSnap,
-  loadSnap,
-} from '../../src/kernel/vatManager/snapStore';
+import { makeSnapstore } from '../../src/kernel/vatManager/snapStore';
 
 const empty = new Uint8Array();
 
@@ -53,25 +48,22 @@ test('build temp file; compress to cache file', async t => {
     ...fs.promises,
   });
   let keepTmp = '';
-  let keepDest = '';
-  await store.withTempName(async name => {
-    keepTmp = name;
-    t.falsy(fs.existsSync(name));
-    fs.writeFileSync(name, 'abc');
-    keepDest = store.resolve('abc.gz');
-    await store.filter(name, createGzip(), keepDest);
+  const hash = await store.save(async fn => {
+    t.falsy(fs.existsSync(fn));
+    fs.writeFileSync(fn, 'abc');
+    keepTmp = fn;
   });
+  t.is(
+    hash,
+    'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+  );
   t.falsy(
     fs.existsSync(keepTmp),
     'temp file should have been deleted after withTempName',
   );
-  t.is(
-    path.resolve(pool, 'abc.gz'),
-    keepDest,
-    'snapStore.resolve works like path.resolve',
-  );
-  t.truthy(fs.existsSync(keepDest));
-  const gz = fs.readFileSync(keepDest);
+  const dest = path.resolve(pool, `${hash}.gz`);
+  t.truthy(fs.existsSync(dest));
+  const gz = fs.readFileSync(dest);
   t.is(gz.toString('hex'), '1f8b08000000000000034b4c4a0600c241243503000000');
 });
 
@@ -99,28 +91,17 @@ test('bootstrap, save, compress', async t => {
     compressed: 190,
   };
 
-  let zfile = '';
-  await store.withTempName(async snapFile => {
+  const h = await store.save(async snapFile => {
     await vat.snapshot(snapFile);
-    t.truthy(
-      fs.existsSync(snapFile),
-      'When a snapshot is taken, we have xsnap write the snapshot to a temporary file',
-    );
-
     t.is(Kb(snapFile), snapSize.raw, 'raw snapshots are large-ish');
-
-    const h = await store.hash(snapFile);
-    t.is(
-      h,
-      'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
-      'snapshots (and their SHA-512 hashes) are deterministic',
-    );
-
-    zfile = store.resolve(`${h}.gz`);
-    await store.atomicWrite(zfile, async ztmp => {
-      await store.filter(snapFile, createGzip(), ztmp);
-    });
   });
+  t.is(
+    h,
+    'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+    'snapshots (and their SHA-512 hashes) are deterministic',
+  );
+
+  const zfile = path.resolve(pool, `${h}.gz`);
   t.is(Kb(zfile), snapSize.compressed, 'compressed snapshots are smaller');
 });
 
@@ -138,11 +119,13 @@ test('create, save, restore, resume', async t => {
   const vat0 = await bootWorker('test', async _ => empty);
   t.teardown(() => vat0.close());
   await vat0.evaluate('globalThis.x = harden({a: 1})');
-  await saveSnap(vat0, store, fs);
+  const h = await store.save(vat0.snapshot);
 
-  const h = 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855';
-  const worker = await loadSnap(store, h, { os: osType(), spawn });
+  t.is(h, 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+  const worker = await store.load(h, async snapshot =>
+    xsnap({ snapshot, os: osType(), spawn }),
+  );
   t.teardown(() => worker.close());
-  worker.evaluate('x.a');
+  await worker.evaluate('x.a');
   t.pass();
 });
