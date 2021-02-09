485e47c437981b0dec709127eacb8a1792a8c3b2
Dan Connolly <connolly@agoric.com>
2021-02-09 13:44:22 -0600

    style(swingset): unused import lint

--- packages/SwingSet/src/kernel/vatManager/manager-subprocess-xsnap.js
+++ packages/SwingSet/src/kernel/vatManager/manager-subprocess-xsnap.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { assert, details } from '@agoric/assert';
+import { assert } from '@agoric/assert';
 import { makeTranscriptManager } from './transcript';
 import { createSyscall } from './syscall';
 
